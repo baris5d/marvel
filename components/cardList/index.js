e0730a644c2cf2771a8c6ad113dc235d067ff065
Barış Dede <baris@barisdede.com>
2021-03-26 07:03:56 +0200

    listing for cards

---
+++ components/cardList/index.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import { useRouter } from "next/router"
+
+import styles from './index.module.css'
+import Card from '../card'
+
+const CardList = ({data}) => {
+    const [characters, setCharacters] = useState([])
+    useEffect(() => {
+      if (data) {
+        if (data.error) {
+        } else {          
+          setCharacters(data)
+        }
+      }
+    }, [data])
+  
+      return(
+        <>
+            {characters.map((character, i) => 
+              <Card src={character.thumbnail.path+"/portrait_uncanny.jpg"} name={character.name} key={i}/>
+            )}
+        </>
+      )
+}
+
+
+export default CardList
